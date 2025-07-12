cf029fc48ae81da0ea5d878cea03ebfef3675e50
Chalaka <chalakajayaranga2@gmail.com>
2025-07-12 12:28:49 +0530

    Add Readme

--- cmlPrintStandard/Print/cmlPrintDocument.Validator.cs
+++ cmlPrintStandard/Print/cmlPrintDocument.Validator.cs
@@ -1,9 +1,6 @@
 ﻿using cmlPrint.TableStructures;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cmlPrint.Print
 {
@@ -11,13 +8,12 @@ namespace cmlPrint.Print
     {
         public void Validate(PrintTable table)
         {
-            // Columns Count
             for (int i = 0; i < table.Rows.Count; i++)
             {
                 PrintTableRow row = table.Rows[i];
                 if (row.Cells == null || row.Cells.Count() != table.Columns)
                     throw new Exception($"Row : ({row}) of Table : ({table}) " +
-                        $"Contains Invalid Amount of Rows");
+                        $"Contains Invalid Amount of Columns");
             }
         }
     }
--- cmlPrintStandard/Print/cmlPrintDocument.cs
+++ cmlPrintStandard/Print/cmlPrintDocument.cs
@@ -1,11 +1,6 @@
 ﻿using cmlPrint.TableStructures;
-using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Printing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cmlPrint.Print
 {
